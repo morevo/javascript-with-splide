256d83a2c0578430e66cfb00a416a9cb37395861
morevo <satanist389@gmail.com>
2021-07-18 01:08:24 +0300

    Finally project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log(2323)
+/* Apply splide */
+document.addEventListener("DOMContentLoaded", function () {
+  const splide = new Splide(".splide", {
+    classes: {
+      arrows: "splide__arrows-position",
+      arrow: "splide__arrow",
+      prev: "splide__arrow--prev",
+      next: "splide__arrow--next",
+    },
+    type: "slide",
+    autoplay: true,
+    interval: 5000,
+    resetProgress: true,
+    pauseOnHover: false,
+    waitForTransition: false,
+    pagination: false,
+    width: "820px",
+    gap: "26px",
+  }).mount();
+});
